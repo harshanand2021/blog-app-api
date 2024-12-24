80ed70cebcc6c7d0b8de0f16e09440bd7cedc0c4
Harsh Anand <harsh25061999@gmail.com>
2024-12-24 22:39:25 +0530

    Spring App initialized

---
+++ src/main/java/com/blog/app/entity/User.java
@@ -0,0 +1,24 @@
+package com.blog.app.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users")
+@NoArgsConstructor
+@Getter
+@Setter
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+
+    @Column(name = "user_name", nullable = false, length = 100)
+    private String name;
+    @Column(nullable = false)
+    private String email;
+    private String password;
+    private String about;
+}
